5e1e2a6424c5167202ce234c6d6b1388eab1d09c
arthursmfarias <arthur.sousa.macedo.farias@ccc.ufcg.edu.br>
2024-09-13 11:07:39 -0300

    entregando insere no final da fila

--- src/main/java/InsereFinalFilaPrioridade.java
+++ src/main/java/InsereFinalFilaPrioridade.java
@@ -9,12 +9,26 @@ public class InsereFinalFilaPrioridade implements FilaPrioridade {
 	}
 	
 	// criar um Pair e adicionar no fim da fila
-	public void add(String elemento, int prioridade) {}
+	public void add(String elemento, int prioridade) {
+		Pair p = new Pair(elemento, prioridade);
+		this.fila.add(p);
+	}
 
 
 	// buscar pelo elemento de maior prioridade na fila.
 	public String removeNext() {
-		return "";
+		int prio = 0;
+		String elem = "";
+		int index = 0;
+		for(int i = 0; i < this.fila.size() - 1; i++){
+			if(this.fila.get(i).getPrioridade() > prio){
+				prio = this.fila.get(i).getPrioridade();
+				elem = this.fila.get(i).getElemento();
+				index = i;
+			}
+		}
+		this.fila.remove(index);
+		return elem;
 	}
 
 }
